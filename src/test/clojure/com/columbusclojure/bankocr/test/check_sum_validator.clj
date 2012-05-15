269e71f301aa638567d583d2dd5f29b4ffc64025
Matthew Boston <matthew.boston@gmail.com>
2012-05-15 17:08:10 -0400

    replace existing CheckSumValidatorTest with a clojure one

--- src/test/clojure/com/columbusclojure/bankocr/test/check_sum_validator.clj
+++ src/test/clojure/com/columbusclojure/bankocr/test/check_sum_validator.clj
@@ -2,5 +2,12 @@
   (:use clojure.test)
   (:import [com.columbusclojure.bankocr CheckSumValidator]))
 
-(deftest CheckSumValidator-test
-  (is true))
+(deftest check-sum-is-valid
+  (is (CheckSumValidator/isValid "345882865"))
+  (is (CheckSumValidator/isValid "000000051"))
+  (is (CheckSumValidator/isValid "123456789"))
+  (is (CheckSumValidator/isValid "000000000")))
+
+(deftest check-sum-is-not-valid
+  (is (not (CheckSumValidator/isValid "123456389")))
+  (is (not (CheckSumValidator/isValid "722466035"))))
