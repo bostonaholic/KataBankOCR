b4e8db9f1208bf5db0ccb0dd2e0500f3ba1e1e01
Matthew Boston <matthew.boston@gmail.com>
2012-05-15 16:40:36 -0400

    first clojure test in a java project

---
+++ src/test/clojure/com/columbusclojure/bankocr/test/check_sum_validator.clj
@@ -0,0 +1,6 @@
+(ns com.columbusclojure.bankocr.test.check-sum-validator
+  (:use clojure.test)
+  (:import [com.columbusclojure.bankocr CheckSumValidator]))
+
+(deftest CheckSumValidator-test
+  (is true))
