ba029159043f4d46cc4cb0e235f01e5df8b70949
Matthew Boston <matthew.boston@gmail.com>
2012-05-15 23:21:13 -0400

    implement the CheckSumValidator in clojure

--- src/main/clojure/com/columbusclojure/bankocr/check_sum_validator.clj
+++ src/main/clojure/com/columbusclojure/bankocr/check_sum_validator.clj
@@ -1,3 +1,6 @@
 (ns com.columbusclojure.bankocr.check-sum-validator)
 
-(defn isValid [^String account] true)
+(defn isValid [^String account]
+  (let [ints (map #(Character/digit % 10) (seq account))
+        sum (reduce + (map-indexed * (cons 0 (reverse ints))))]
+    (= (mod sum 11) 0)))
