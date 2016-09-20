5334b21c9d68f4853c983ffbb9e858f43dbeddd4
Matthew Boston <matthew@matthewboston.com>
2016-09-19 19:21:16 -0600

    add a failing test

---
+++ src/test/java/com/columbusclojure/bankocr/FailTest.java
@@ -0,0 +1,12 @@
+package com.columbusclojure.bankocr;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class FailTest {
+    @Test
+    public void testFail() {
+        fail("fail");
+    }
+}
